f7245bc8858c33ee05297a2ae9247aeef7f51e51
margaritasing <libiamfreitesm@gmail.com>
2023-02-20 15:11:17 -0300

    subo cambios

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools  } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
@@ -7,8 +9,13 @@ import { router } from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider client={ client }>
+      <ReactQueryDevtools/>
+       <RouterProvider router={ router } />
+    </QueryClientProvider>
   </React.StrictMode>
 )
